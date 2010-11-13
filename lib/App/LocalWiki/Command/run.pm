8e374c37b80ba2e632195c0f75579f8b80a63f86
Chris Weyl <cweyl@alumni.drew.edu>
2010-11-12 21:28:31 -0800

    Add pixmaps, for our viewing pleasure
    This took a while to figure out, but apparently we load all our pixbufs at the beginning, and make
    those available as stock icons.

--- lib/App/LocalWiki/Command/run.pm
+++ lib/App/LocalWiki/Command/run.pm
@@ -4,6 +4,7 @@ use Moose;
 use namespace::autoclean;
 use common::sense;
 use App::LocalWiki::Types ':all';
+use Path::Class;
 
 extends 'MooseX::App::Cmd::Command';
 
@@ -26,6 +27,8 @@ sub execute {
     # FIXME this may need to take place before loading other packages
     Gtk2->init();
 
+    $self->load_pixmaps();
+
     #my $win = App::LocalWiki::Window::Main->new();
     my $win = $self->app->main_window_class->new(app => $self->app);
     #$win->widget->show();
@@ -33,6 +36,34 @@ sub execute {
 
     Gtk2->main();
     ...
+}
+
+sub load_pixmaps {
+    my $self = shift @_;
+
+    my $dir = dir qw{ share pixmaps };
+
+    my @files;
+    while (my $file = $dir->next) {
+
+        push @files, $file->absolute unless $file->is_dir;
+        warn "$file";
+    }
+
+	my $factory = Gtk2::IconFactory->new;
+	$factory->add_default;
+	my %icons;
+	for my $f (@files) {
+		my $pixbuf = Gtk2::Gdk::Pixbuf->new_from_file("$f") || die $@;
+		my $icon_set = Gtk2::IconSet->new_from_pixbuf($pixbuf) || die $@;
+		my $n = "$f";
+		$n =~ s/.*[\/\\]//;
+		$n =~ s/\..*//;
+		#next if exists $icons{$n};
+		$icons{"$n"} = "$f";
+		warn "Icon: $n => $f\n";
+		$factory->add($n => $icon_set);
+	}
 }
 
 __PACKAGE__->meta->make_immutable;
