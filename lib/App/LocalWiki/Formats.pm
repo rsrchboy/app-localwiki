fe416f67cb40a7f1848a41981e544cc3d6685665
Chris Weyl <cweyl@alumni.drew.edu>
2011-04-30 21:33:49 -0700

    shift over to Dist::Zilla

--- lib/App/LocalWiki/Formats.pm
+++ lib/App/LocalWiki/Formats.pm
@@ -1,30 +1,13 @@
-#
-#===============================================================================
-#
-#         FILE:  Formats.pm
-#
-#  DESCRIPTION:  
-#
-#        FILES:  ---
-#         BUGS:  ---
-#        NOTES:  ---
-#       AUTHOR:  YOUR NAME (), 
-#      COMPANY:  
-#      VERSION:  1.0
-#      CREATED:  09/29/2010 07:13:49 PM
-#     REVISION:  ---
-#===============================================================================
+package App::LocalWiki::Formats;
 
 use Moose;
-use namspace::autoclean;
+use namespace::autoclean;
 use common::sense;
 
-our $VERSION = '0.000_01';
 
-# uhmmmm....  keep it simple
-
-list
-get
-
+# list
+# get
 
 __PACKAGE__->meta->make_immutable;
+
+1;
